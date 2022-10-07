5af741ffab1a4f55ec149f4d74bf6161ba69bcc8
Andrey Popov <andrvpopov@gmail.com>
2022-10-07 22:23:54 +0300

    Add task04

---
+++ Task02/guess_number/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ma_karov/guess-number',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5465112a059c3ea054ef33ff931fdadc75950763',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ma_karov/guess-number' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5465112a059c3ea054ef33ff931fdadc75950763',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.11.15',
+            'version' => '0.11.15.0',
+            'reference' => 'b6edd35988892ea1451392eb7a26d9dbe98c836d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
