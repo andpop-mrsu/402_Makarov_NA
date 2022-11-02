328b4fd585df6cf3e775787a5c4063cd7fca4065
Andrey Popov <andrvpopov@gmail.com>
2022-11-02 14:45:47 +0300

    Add task06

---
+++ Task05/guess_number/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '085c8d9ff2e51f2b6ada8ef5100961b9e0f94edc',
+        'name' => 'ma_karov/guess_number',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gabordemooij/redbean' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gabordemooij/redbean',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'e91adf5d25b0e7a354c3a8ee943cecaef323cc5c',
+            'dev_requirement' => false,
+        ),
+        'ma_karov/guess_number' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '085c8d9ff2e51f2b6ada8ef5100961b9e0f94edc',
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.11.15',
+            'version' => '0.11.15.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'reference' => 'b6edd35988892ea1451392eb7a26d9dbe98c836d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
